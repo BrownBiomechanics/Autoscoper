3abbd48c915f2c568173fde448db69e602c83f2b
Bardiya_Ak <bardiya.akhbari@gmail.com>
2019-09-16 11:39:32 -0400

    Added installer and modified some limitation of sizes in windows.

--- autoscoper/src/ui/AutoscoperMainWindow.cpp
+++ autoscoper/src/ui/AutoscoperMainWindow.cpp
@@ -1,5 +1,5 @@
 // ----------------------------------
-// Copyright (c) 2011, Brown University
+// Copyright (c) 2011-2019, Brown University
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
