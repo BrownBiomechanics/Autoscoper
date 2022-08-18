6c933dc51248b0cd47adeda36b6592b2d8779c09
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2022-08-18 15:54:30 -0400

    STYLE: Update CMake sources to use consistent indentation and spacing
    Summary of changes:
    * Use 2 spaces for block indentation
    * Remove redundant empty lines
    * Remove extra spaces in CMake function, macro and command calls
    
    Co-authored-by: Anthony Lombardi <anthony.lombardi@kitware.com>

--- cmake/MessageMacros.cmake
+++ cmake/MessageMacros.cmake
@@ -1,7 +1,6 @@
 # This file was taken from the MinVR cmake build system. https://github.com/MinVR/MinVR
 # See the main MinVR/CMakeLists.txt file for authors, copyright, and license info.
 
-
 macro(h1 TITLE)
   string(TOUPPER ${TITLE} TITLE)
   message(STATUS "\n\n==== ${TITLE} ====")
