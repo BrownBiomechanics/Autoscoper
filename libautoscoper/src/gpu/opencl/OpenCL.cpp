140666d2c474c595507744cafc47bfde3d23152c
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2022-11-30 17:55:43 -0500

    ENH: Update opencl_error() adding support for OpenCL 2.0 and 2.2

--- libautoscoper/src/gpu/opencl/OpenCL.cpp
+++ libautoscoper/src/gpu/opencl/OpenCL.cpp
@@ -178,6 +178,14 @@ static const char* opencl_error(cl_int err)
     case CL_INVALID_LINKER_OPTIONS: return "CL_INVALID_LINKER_OPTIONS";
     case CL_INVALID_DEVICE_PARTITION_COUNT: return "CL_INVALID_DEVICE_PARTITION_COUNT";
 #endif
+#ifdef CL_VERSION_2_0
+    case CL_INVALID_PIPE_SIZE: return "CL_INVALID_PIPE_SIZE";
+    case CL_INVALID_DEVICE_QUEUE: return "CL_INVALID_DEVICE_QUEUE";
+#endif
+#ifdef CL_VERSION_2_2
+    case CL_INVALID_SPEC_ID: return "CL_INVALID_SPEC_ID";
+    case CL_MAX_SIZE_RESTRICTION_EXCEEDED: return "CL_MAX_SIZE_RESTRICTION_EXCEEDED";
+#endif
     default: return "Unknown";
     }
 }
