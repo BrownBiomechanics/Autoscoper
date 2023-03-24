BrownBiomechanics/Autoscoper

commit e1d3d493505108a0dbc6da4c6a551accbc701896

COMP: Update GLEW external project to download sources using Git
Anticipating the integration of build-system changes specific to
Autoscoper, this commit updates the corresponding external project
to checkout GLEW sources from the BrownBiomechanics GitHub fork.

To support directly building the tag associated with "glew-2.2.0" release,
additional generated files were included.

Copied from BrownBiomechanics/glew@7cffe085f commit message:

    Copy generated sources GL/*.h and src/*.c from glew-2.2.0 release

    To conveniently support building after downloading sources from
    the Git repository, this commit copies the generated headers and
    sources found in the release asset "glew-2.2.0.tar.gz" associated
    with nigels-com/glew@glew-2.2.0 (nigels-com/glew@9fb23c3).

    See https://github.com/nigels-com/glew/releases/tag/glew-2.2.0

    $ md5sum glew-2.2.0.tg
    3579164bccaef09e36c0af7f4fd5c7c7  glew-2.2.0.tgz

List of glew changes:

    $ git shortlog 9fb23c3e6..7cffe085f
    Jean-Christophe Fillion-Robin (1):
          Copy generated sources GL/*.h and src/*.c from glew-2.2.0 release

Superbuild/External_GLEW.cmake
@@ -31,8 +31,8 @@ if(NOT DEFINED GLEW_DIR AND NOT Autoscoper_USE_SYSTEM_${proj})
 
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
-    URL https://github.com/nigels-com/glew/releases/download/glew-2.2.0/glew-2.2.0.zip
-    URL_MD5 970535b75b1b69ebd018a0fa05af63d1
+    GIT_REPOSITORY https://github.com/BrownBiomechanics/glew.git
+    GIT_TAG 7cffe085f72ad57a535eb0812b7f3363f02d492b # autoscoper-2.2.0-2020-03-15-9fb23c3e6
     SOURCE_DIR ${EP_SOURCE_DIR}
     SOURCE_SUBDIR build/cmake
     BINARY_DIR ${EP_BINARY_DIR}

commit 8f89e5b09158e1f73e965c74f32d02de866db1c7

COMP: Update GLEW to fix windows build regression
This commit fixes a regression introduced in e1d3d49 (COMP: Update GLEW
external project to download sources using Git)

List of changes:

$ git shortlog 62d671f..8248234 --no-merges
Jean-Christophe Fillion-Robin (1):
      Fix windows build copying generated *.rc files from glew-2.2.0 release

Superbuild/External_GLEW.cmake
@@ -26,7 +26,7 @@ if(NOT DEFINED GLEW_DIR AND NOT Autoscoper_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY https://github.com/BrownBiomechanics/glew.git
-    GIT_TAG 62d671ff5c6e24ec590026810cbea619c549d5e4 # autoscoper-2.2.0-2020-03-15-9fb23c3e6
+    GIT_TAG 8248234b584a17528a499610c9dec13db8c9acba # autoscoper-2.2.0-2020-03-15-9fb23c3e6
     SOURCE_DIR ${EP_SOURCE_DIR}
     SOURCE_SUBDIR build/cmake
     BINARY_DIR ${EP_BINARY_DIR}