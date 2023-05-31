05ce8948df779d29966c154102529507971fa5f5
Anthony Lombardi <anthony.lombardi@kitware.com>
2023-05-31 10:35:25 -0400

    ENH: Make content take up 100% of the screen width

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -48,3 +48,12 @@ html_theme_options = {
     "logo_only": True,
     "display_version": False,
 }
+
+
+html_static_path = ["_static"]
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    "css/custom.css",
+]
