BrownBiomechanics/Autoscoper

commit 2f7ae7f832174298a6d45e51347cd2cf4ced7d88

ENH: Add Error checking to socket

scripts/python/PyAutoscoper/connect.py
@@ -357,7 +357,17 @@ class AutoscoperConnection:
         return [width, height, img_data]
 
     def optimizeFrame(
-        self, volume, frame, repeats, max_itr, min_lim, max_lim, max_stall_itr
+        self,
+        volume,
+        frame,
+        repeats,
+        max_itr,
+        min_lim,
+        max_lim,
+        max_stall_itr,
+        dframe,
+        opt_method,
+        cf_model,
     ):
         """
         Optimize the pose of the volume at the specified frame.
@@ -376,8 +386,18 @@ class AutoscoperConnection:
         :type max_lim: float
         :param max_stall_itr: The maximum number of iterations to stall
         :type max_stall_itr: int
+        :param dframe: The amount of frames to skip
+        :type dframe: int
+        :param opt_method: The optimization method to use, 0 for Particle Swarm, 1 for Downhill Simplex
+        :type opt_method: int
+        :param cf_model: The cost function model to use, 0 for NCC (Bone Models), 1 for Sum of Absolute Differences (Implant Models)
+        :type cf_model: int
         :raises Exception: If the server fails to optimize the frame
         """
+        if opt_method not in [0, 1]:
+            raise Exception("Invalid optimization method")
+        if cf_model not in [0, 1]:
+            raise Exception("Invalid cost function model")
         if self.verbose:
             print(f"Optimizing volume {volume} on frame {frame}")
         b = bytearray()
@@ -389,6 +409,9 @@ class AutoscoperConnection:
         b.extend(struct.pack("d", min_lim))
         b.extend(struct.pack("d", max_lim))
         b.extend(max_stall_itr.to_bytes(4, byteorder="little", signed=False))
+        b.extend(dframe.to_bytes(4, byteorder="little", signed=False))
+        b.extend(opt_method.to_bytes(4, byteorder="little", signed=False))
+        b.extend(cf_model.to_bytes(4, byteorder="little", signed=False))
         self.socket.sendall(b)
         res = self.wait_for_server()
         if int.from_bytes(res, byteorder="little", signed=False) != 0x0B:
@@ -415,10 +438,11 @@ class AutoscoperConnection:
 
         """
         b = bytearray()
-        b.append(0xFF)
+        # convert 13 to bytes
+        b.append(0x0D)
         self.socket.sendall(b)
         res = self.wait_for_server()
-        if int.from_bytes(res, byteorder="little", signed=False) != 0xFF:
+        if int.from_bytes(res, byteorder="little", signed=False) != 0x0D:
             raise Exception("Server Error closing connection")
 
     def trackingDialog(
@@ -432,6 +456,8 @@ class AutoscoperConnection:
         min_lim=-3,
         max_lim=3,
         max_stall_itr=25,
+        opt_method=0,
+        cf_model=0,
     ):
         """
         Automatically tracks the volume accross the given frames.
@@ -461,7 +487,7 @@ class AutoscoperConnection:
             print(
                 f"Automated tracking of volume {volume} from frame {start_frame} to {end_frame}.\n"
             )
-        for frame in range(start_frame, end_frame, frame_skip):
+        for frame in range(start_frame, end_frame):
             self.setFrame(frame=frame)
             if frame != 0:
                 pose = self.getPose(volume=volume, frame=(frame - 1))
@@ -474,4 +500,7 @@ class AutoscoperConnection:
                 min_lim=min_lim,
                 max_lim=max_lim,
                 max_stall_itr=max_stall_itr,
+                opt_method=opt_method,
+                cf_model=cf_model,
+                dframe=frame_skip,
             )

.github/workflows/black.yml
@@ -9,6 +9,5 @@ jobs:
       - uses: actions/checkout@v2
       - uses: psf/black@stable
         with:
-          options: "--check --verbose"
           src: "./scripts/python/"
           version: "~= 22.0"

autoscoper/src/net/Socket.cpp
@@ -45,6 +45,7 @@
 
 #include "Socket.h"
 #include <iostream>
+#include <fstream>
 #include <QTcpSocket>
 
 Socket::Socket(AutoscoperMainWindow* mainwindow, unsigned long long int listenPort) : m_mainwindow(mainwindow)
@@ -72,11 +73,17 @@ void Socket::handleMessage(QTcpSocket * connection, char* data, qint64 length)
     {
       //load trial
       std::string filename = std::string(&data[1],length-1);
+      std::ifstream test(filename.c_str());
+      if (!test) {
+          std::cerr << "Cannot find " << filename.c_str() << std::endl;
+          connection->write(QByteArray(1, 0));
+      }
+      else {
+          std::cerr << "load trial " << filename.c_str() << std::endl;
+          m_mainwindow->openTrial(QString(filename.c_str()));
 
-      std::cerr << "load trial " << filename.c_str() << std::endl;
-      m_mainwindow->openTrial(QString(filename.c_str()));
-
-      connection->write(QByteArray(1, 1));
+          connection->write(QByteArray(1, 1));
+      }
     }
     break;
   case 2:
@@ -90,12 +97,19 @@ void Socket::handleMessage(QTcpSocket * connection, char* data, qint64 length)
       qint32* convert_to_rad = reinterpret_cast<qint32*>(&data[21]);
       qint32* interpolate = reinterpret_cast<qint32*>(&data[25]);
       std::string filename = std::string(&data[29], length - 29);
-      std::cerr << "load tracking data Volume " << *volume << " : " << filename.c_str() << std::endl;
-      std::cerr << "Save as matrix: " << *save_as_matrix << " save as rows: " << *save_as_rows << " save with commas: " << *save_with_commas << " convert to cm: " << *convert_to_cm << " convert to rad: " << *convert_to_rad << " interpolate: " << *interpolate << std::endl;
+      std::ifstream test(filename.c_str());
+      if (!test) {
+          std::cerr << "Cannot find " << filename.c_str() << std::endl;
+          connection->write(QByteArray(1, 0));
+      }
+      else {
+          std::cerr << "load tracking data Volume " << *volume << " : " << filename.c_str() << std::endl;
+          std::cerr << "Save as matrix: " << *save_as_matrix << " save as rows: " << *save_as_rows << " save with commas: " << *save_with_commas << " convert to cm: " << *convert_to_cm << " convert to rad: " << *convert_to_rad << " interpolate: " << *interpolate << std::endl;
 
-      m_mainwindow->load_tracking_results(QString(filename.c_str()), *save_as_matrix, *save_as_rows, *save_with_commas, *convert_to_cm, *convert_to_rad, *interpolate, *volume);
+          m_mainwindow->load_tracking_results(QString(filename.c_str()), *save_as_matrix, *save_as_rows, *save_with_commas, *convert_to_cm, *convert_to_rad, *interpolate, *volume);
 
-      connection->write(QByteArray(1, 2));
+          connection->write(QByteArray(1, 2));
+      }
     }
     break;
   case 3:
@@ -109,13 +123,20 @@ void Socket::handleMessage(QTcpSocket * connection, char* data, qint64 length)
       qint32* convert_to_rad = reinterpret_cast<qint32*>(&data[21]);
       qint32* interpolate = reinterpret_cast<qint32*>(&data[25]);
       std::string filename = std::string(&data[29], length - 29);
+      std::ifstream test(filename.c_str());
+      if (!test) {
+          std::cerr << "Cannot find " << filename.c_str() << std::endl;
+          connection->write(QByteArray(1, 0));
+      }
+      else {
 
-      std::cerr << "save tracking data Volume " << *volume << " : " << filename.c_str() << std::endl;
-      std::cerr << "Save as matrix: " << *save_as_matrix << " save as rows: " << *save_as_rows << " save with commas: " << *save_with_commas << " convert to cm: " << *convert_to_cm << " convert to rad: " << *convert_to_rad << " interpolate: " << *interpolate << std::endl;
+          std::cerr << "save tracking data Volume " << *volume << " : " << filename.c_str() << std::endl;
+          std::cerr << "Save as matrix: " << *save_as_matrix << " save as rows: " << *save_as_rows << " save with commas: " << *save_with_commas << " convert to cm: " << *convert_to_cm << " convert to rad: " << *convert_to_rad << " interpolate: " << *interpolate << std::endl;
 
-      m_mainwindow->save_tracking_results(QString(filename.c_str()), *save_as_matrix, *save_as_rows, *save_with_commas, *convert_to_cm, *convert_to_rad, *interpolate, *volume);
+          m_mainwindow->save_tracking_results(QString(filename.c_str()), *save_as_matrix, *save_as_rows, *save_with_commas, *convert_to_cm, *convert_to_rad, *interpolate, *volume);
 
-      connection->write(QByteArray(1, 3));
+          connection->write(QByteArray(1, 3));
+      }
     }
     break;
   case 4:
@@ -123,11 +144,18 @@ void Socket::handleMessage(QTcpSocket * connection, char* data, qint64 length)
     {
       qint32* camera = reinterpret_cast<qint32*>(&data[1]);
       std::string filename = std::string(&data[5], length - 5);
+      std::ifstream test(filename.c_str());
+      if (!test) {
+          std::cerr << "Cannot find " << filename.c_str() << std::endl;
+          connection->write(QByteArray(1, 0));
+      }
+      else {
 
-      std::cerr << "load filter settings for camera " << *camera << " : " << filename.c_str() << std::endl;
-      m_mainwindow->loadFilterSettings(*camera, QString(filename.c_str()));
+          std::cerr << "load filter settings for camera " << *camera << " : " << filename.c_str() << std::endl;
+          m_mainwindow->loadFilterSettings(*camera, QString(filename.c_str()));
 
-      connection->write(QByteArray(1, 4));
+          connection->write(QByteArray(1, 4));
+      }
     }
     break;
   case 5:
@@ -240,16 +268,16 @@ void Socket::handleMessage(QTcpSocket * connection, char* data, qint64 length)
       double* max_limit = reinterpret_cast<double*>(&data[25]);
       qint32* stall_iter = reinterpret_cast<qint32*>(&data[33]);
 
-      qint32 dframe = 1;// reinterpret_cast<qint32*>(&data[5]);
-      qint32 opt_method = 0;//reinterpret_cast<qint32*>(&data[25]);
-      qint32 cf_model = 0;//reinterpret_cast<qint32*>(&data[41]);
+      qint32* dframe = reinterpret_cast<qint32*>(&data[37]);
+      qint32* opt_method = reinterpret_cast<qint32*>(&data[41]);
+      qint32* cf_model = reinterpret_cast<qint32*>(&data[45]);
 
       std::cerr << "Running optimization from autoscoper for frame #" << *frame << std::endl;
 
-      m_mainwindow->optimizeFrame(*volumeID, *frame, dframe, *repeats,
-        opt_method,
+      m_mainwindow->optimizeFrame(*volumeID, *frame, *dframe, *repeats,
+        *opt_method,
         *max_iter, *min_limit, *max_limit,
-        cf_model, *stall_iter);
+        *cf_model, *stall_iter);
 
       connection->write(QByteArray(1, 11));
     }
@@ -269,7 +297,7 @@ void Socket::handleMessage(QTcpSocket * connection, char* data, qint64 length)
   case 13:
     // close connection
     {
-      std::cerr << "Closing connection to Matlab Client..." << std::endl;
+      std::cerr << "Closing connection to Client..." << std::endl;
       connection->write(QByteArray(1, 13));
       deleteConnection();
     }

commit 9667fee9793564b77bec4b13b6ff50b92617ed40

BUG: Fix invocation of optimizeFrame in PyAutoscoper trackingDialog
This commit fixes a regression originally introduced in
2f7ae7f83 (ENH: Add Error checking to socket)

Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

scripts/python/PyAutoscoper/connect.py
@@ -557,9 +557,9 @@ class AutoscoperConnection:
                 min_lim=min_lim,
                 max_lim=max_lim,
                 max_stall_itr=max_stall_itr,
+                dframe=frame_skip,
                 opt_method=opt_method,
                 cf_model=cf_model,
-                dframe=frame_skip,
             )
 
     def getNumVolumes(self):