a64ba03fa6bf82166f4d040326e659d0eb6519bd
Anthony Lombardi <anthony.lombardi@kitware.com>
2023-07-27 12:12:06 -0400

    ENH: Add Enums to MATLAB

---
+++ scripts/matlab/CostFunction.m
@@ -0,0 +1,6 @@
+classdef CostFunction < int32
+    enumeration
+        NORMALIZED_CROSS_CORRELATION (0)
+        SUM_OF_ABSOLUTE_DIFFERENCES (1)
+    end
+end
---
+++ scripts/matlab/OptimizationMethod.m
@@ -0,0 +1,6 @@
+classdef OptimizationMethod < int32
+    enumeration
+        PARTICLE_SWARM_OPTIMIZATION (0)
+        DOWNHILL_SIMPLEX (1)
+    end
+end
