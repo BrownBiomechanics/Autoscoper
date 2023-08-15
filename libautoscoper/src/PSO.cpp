7e220b9a4e57421e57fb6f6331bdfa3b11d9bb9d
Jacob Moore <jacob.moore@kitware.com>
2023-08-15 12:33:04 -0400

    ENH: Adding optionion velocity limiting and collision response, WIP

--- libautoscoper/src/PSO.cpp
+++ libautoscoper/src/PSO.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <string>
 
+#define VELOCITY_FILTER 0
+#define COLLISION_REPONSE 0
 
 // New Particle Swarm Optimization
 float host_fitness_function(float x[])
@@ -41,6 +43,8 @@ float getRandom(float low, float high)
   return low + (high - low + 1.0f)*(float)rand() / (RAND_MAX + 1.0f);
 }
 
+
+
 float getRandomClamped()
 {
   return (float)rand() / (float)RAND_MAX;
@@ -56,11 +60,21 @@ void pso(float *positions, float *velocities, float *pBests, float *gBest, unsig
   unsigned int counter = 0;
   //for (int iter = 0; iter < (signed int)MAX_EPOCHS; iter++)
 
-  // Inertial weight of PSO. Measure of how likley a particle is to remail
+
+  float clamp = 0.2;
+
+  // Inertial weight of PSO. Measure of how likley a particle is to remain
   // traveling in the same direciton it has been traveling (momentum like)
-  float OMEGA = 0.5f;
+  float OMEGA = 0.8f;
 
   float OMEGA_MIN = 0.0001;
+
+
+  float avgNonCollidedPosition[NUM_OF_DIMENSIONS];
+  float avgCollidedPosition[NUM_OF_DIMENSIONS];
+  float correctionVec[NUM_OF_DIMENSIONS];
+
+  bool collided[NUM_OF_PARTICLES];
 
   while (do_this)
   {
@@ -78,20 +92,33 @@ void pso(float *positions, float *velocities, float *pBests, float *gBest, unsig
       float rg = getRandomClamped();
 
       velocities[i] = OMEGA * velocities[i] + c1 * rp*(pBests[i] - positions[i]) + c2 * rg*(gBest[i%NUM_OF_DIMENSIONS] - positions[i]);
+#if VELOCITY_FILTER
+      velocities[i] = std::max(-clamp, std::min(velocities[i], clamp));
+#endif
+
 
       positions[i] += velocities[i];
     }
-    // Note: For better performance, consider changing this to be linearly decreasing, with a floor at w=0.4 (DOI: 10.1080/0952813X.2013.782348)
+    // Note: For possible better performance, consider changing this to be linearly decreasing, with a floor at w=0.4 (DOI: 10.1080/0952813X.2013.782348)
     OMEGA = OMEGA * 0.9f;
 
-    /*float dOmega = (OMEGA - OMEGA_MIN) / 30.0;
-    OMEGA = OMEGA - dOmega;
-
-    OMEGA = std::max(OMEGA, OMEGA_MIN);*/
-
     float globalBest = currentBest;
+#if COLLISION_RESPONSE
+    int collidedCount = 0;
 
-    for (int i = 0; i < NUM_OF_PARTICLES*NUM_OF_DIMENSIONS; i += NUM_OF_DIMENSIONS)
+    for (int j = 0; j < NUM_OF_DIMENSIONS; j++) {
+      avgNonCollidedPosition[j] = 0.0;
+      avgCollidedPosition[j] = 0.0;
+    }
+    
+    for (int j = 0; j < NUM_OF_PARTICLES; j++)
+    {
+      collided[j] = false;
+    }
+#endif
+
+    int particleId = 0;
+    for (int i = 0; i < NUM_OF_PARTICLES * NUM_OF_DIMENSIONS; i += NUM_OF_DIMENSIONS)
     {
       for (int j = 0; j < NUM_OF_DIMENSIONS; j++)
       {
@@ -99,12 +126,30 @@ void pso(float *positions, float *velocities, float *pBests, float *gBest, unsig
         tempParticle2[j] = pBests[i + j];
       }
 
-      // Normalized cross correlation for this particle now
+      // Normalized cross correlation for this particles current position
       float nccP = host_fitness_function(tempParticle1);
+
+#if COLLISION_RESPONSE
+      if (nccP > 1.0E4) {
+        collidedCount++;
+
+        collided[particleId] = true;
+        for (int j = 0; j < NUM_OF_DIMENSIONS; j++) {
+          avgCollidedPosition[j] += tempParticle1[j];
+        }
+      }
+      
+      if (nccP < 1.0E4){
+        
+        for (int j = 0; j < NUM_OF_DIMENSIONS; j++) {
+          avgNonCollidedPosition[j] += tempParticle1[j];
+        }
+      }
+#endif
+
       // Normalized cross correlation for this particle in its best position so far
       float nccPBest = host_fitness_function(tempParticle2);
 
-      // if (host_fitness_function(tempParticle1) < host_fitness_function(tempParticle2))
       if (nccP < nccPBest)
       {
         for (int j = 0; j < NUM_OF_DIMENSIONS; j++)
@@ -123,13 +168,48 @@ void pso(float *positions, float *velocities, float *pBests, float *gBest, unsig
           globalBest = nccP;
         }
       }
+      particleId++;
     }
+#if COLLISION_RESPONSE
+    if (collidedCount != 0 && collidedCount != 100) {
+
+      // std::cout << "Correction Vector: ";
+      for (int j = 0; j < NUM_OF_DIMENSIONS; j++) {
+
+
+        avgNonCollidedPosition[j] /= (float)NUM_OF_PARTICLES - (float)collidedCount;
+        avgCollidedPosition[j] /= (float)collidedCount;
+        
+        correctionVec[j] = avgNonCollidedPosition[j] - avgCollidedPosition[j];
+
+      }
+    }
+
+    if (collidedCount != 0 && collidedCount != 100) {
+      particleId = 0;
+      for (int i = 0; i < NUM_OF_PARTICLES * NUM_OF_DIMENSIONS; i += NUM_OF_DIMENSIONS)
+      {
+        if (collided[particleId] == true) {
+
+          // std::cout << "Shifted particles = " << particleId << std::endl;
+          for (int j = 0; j < NUM_OF_DIMENSIONS; j++)
+          {
+            positions[i + j] += ((float)collidedCount/(float)NUM_OF_PARTICLES)*correctionVec[j];
+            pBests[i + j] = positions[i + j];
+          }
+        }
+
+        particleId++;
+      }
+    }
+#endif
 
     float epochBest = globalBest; 
     // float epochBest = host_fitness_function(gBest);
 
-
-    if (counter % 5 == 0) std::cout << "Current Best NCC: " << epochBest << std::endl;
+    
+    
+    if (counter % 5 == 0) std::cout << "Current Best NCC: " << epochBest << "   on iteration:"<< counter <<" and stall = "<< stall_iter << std::endl;
     //std::cout << "Stall: " << stall_iter << std::endl;
     if (abs(epochBest - currentBest) < 1e-4f)
     {
