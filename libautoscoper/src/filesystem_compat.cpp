701f92b8cab1ae06181875356d2c4cf76c67c7c9
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-08-30 02:51:15 -0400

    COMP: Move compat version of "relative" to "filesystem_compat.h"
    Add compat version of the "relative" function to the "xromm::filesystem"
    namespace and add test checking the function works.
    
    Since the xromm::filesystem::relative compatibility function is not
    expected to work on Windows, explicitly mark the text as expected to fail.

---
+++ libautoscoper/src/filesystem_compat.cpp
@@ -0,0 +1,47 @@
+#include "filesystem_compat.hpp"
+
+namespace xromm {
+    namespace filesystem {
+        // Experimental filesystem compliant version where there is no fs::relative
+        std::filesystem::path relative(
+           const std::filesystem::path& path,
+           const std::filesystem::path& basePath)
+        {
+            // find the first mismatched element and the shared root
+            auto mismatched = std::mismatch(path.begin(), path.end(), basePath.begin(), basePath.end());
+
+            // If the paths are the same, return "."
+            if (mismatched.first == path.end() && mismatched.second == basePath.end()) {
+              return ".";
+            }
+
+            std::filesystem::path relativePath;
+            auto pathIt = mismatched.first;
+            auto baseIt = mismatched.second;
+
+            // Move the base to the shared root
+            for (; baseIt != basePath.end(); ++baseIt) {
+              relativePath /= "..";
+            }
+
+            // Add the remaining path
+            for (; pathIt != path.end(); ++pathIt) {
+              relativePath /= *pathIt;
+            }
+
+            return relativePath.string();
+        }
+    }
+}
+
+#if Autoscoper_HAS_EXPERIMENTAL_FILESYSTEM
+namespace std {
+    namespace experimental::filesystem {
+        std::filesystem::path relative(
+          const std::filesystem::path& path,
+          const std::filesystem::path& basePath){
+            return xromm::filesystem::relative(path, basePath);
+          }
+    }
+}
+#endif
