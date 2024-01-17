BrownBiomechanics/Autoscoper

commit 73eb359dcd70878c4b4c28471fd2684c08bd3014

COMP: Fix CUDA RayCaster not having setVisible member
* error C2039: 'setVisible': is not a member of 'xromm::gpu::RayCaster'
* Regression introduced in 103df1a

libautoscoper/src/gpu/cuda/RayCaster.cpp
@@ -46,6 +46,7 @@
 #include "RayCaster.hpp"
 #include "RayCaster_kernels.h"
 #include "VolumeDescription.hpp"
+#include <cuda_runtime_api.h>
 
 namespace xromm { namespace gpu {
 
@@ -133,6 +134,11 @@ RayCaster::render(float* buffer, size_t width, size_t height)
 {
     if (!volumeDescription_) {
         std::cerr << "RayCaster: WARNING: No volume loaded. " << std::endl;
+        return;
+    }
+
+    if (!visible_) {
+        cudaMemset(buffer, 0, width * height * sizeof(float));
         return;
     }
 

libautoscoper/src/gpu/cuda/RayCaster.hpp
@@ -103,6 +103,9 @@ public:
         name_ = name;
     }
 
+    void setVisible(bool visible) {
+        visible_ = visible;
+    }
 private:
 
     VolumeDescription* volumeDescription_;
@@ -118,6 +121,8 @@ private:
     float cutoff_;
 
     std::string name_;
+
+    bool visible_;
 };
 
 } } // namespace xromm::cuda

commit 0b83e4cc14ffb535292e6583082191b4495d7528

BUG: Ensure CUDA RayCaster is sets visible to true by default
Bug was introduced in commit 73eb359dcd70878c4b4c28471fd2684c08bd3014

libautoscoper/src/gpu/cuda/RayCaster.cpp
@@ -66,6 +66,7 @@ RayCaster::RayCaster() : volumeDescription_(0),
     std::stringstream name_stream;
     name_stream << "DrrRenderer" << (++num_ray_casters);
     name_ = name_stream.str();
+    visible_ = true;
 
     viewport_[0] = -1.0f;
     viewport_[1] = -1.0f;