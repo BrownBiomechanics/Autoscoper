BrownBiomechanics/Autoscoper

commit 9bbce52122bce0584dcdbd5ac88202344010f657

background mask 1

autoscoper/src/ui/AutoscoperMainWindow.cpp
@@ -91,7 +91,7 @@
 
 AutoscoperMainWindow::AutoscoperMainWindow(bool skipGpuDevice, QWidget *parent) :
 												QMainWindow(parent),
-												ui(new Ui::AutoscoperMainWindow){
+												ui(new Ui::AutoscoperMainWindow), background_threshold_(-1.0){
 	
 	//Setup UI
 	ui->setupUi(this);
@@ -123,7 +123,7 @@ AutoscoperMainWindow::AutoscoperMainWindow(bool skipGpuDevice, QWidget *parent) 
 		else{ xromm::gpu::setUsedPlatform(0); }
 		delete dialog;
 }
-
+#endif
 	timeline_widget =  new TimelineDockWidget(this);
 	this->addDockWidget(Qt::BottomDockWidgetArea, timeline_widget);
 	
@@ -137,7 +137,6 @@ AutoscoperMainWindow::AutoscoperMainWindow(bool skipGpuDevice, QWidget *parent) 
 	worldview->hide();
 
 	setupShortcuts();
-#endif
 }
 
 AutoscoperMainWindow::~AutoscoperMainWindow(){
@@ -296,6 +295,7 @@ void AutoscoperMainWindow::frame_changed()
 
 void AutoscoperMainWindow::volume_changed()
 {
+	
 	// Lock or unlock the position
 	if (timeline_widget->getPosition_graph()->frame_locks.at(tracker->trial()->frame)) {
 		timeline_widget->setValuesEnabled(false);
@@ -1464,6 +1464,20 @@ void AutoscoperMainWindow::on_actionDelete_triggered(bool checked){
 
 		redrawGL();
     }
+}
+
+void AutoscoperMainWindow::on_actionSet_Background_triggered(bool checked)
+{
+	bool ok;
+	double threshold = QInputDialog::getDouble(this, "Enter threshold", "threshold in percent", 0.2, 0.0, 1.0, 0.01, &ok);
+	if (ok){
+		if (background_threshold_ < 0){
+			for (xromm::Video vi : tracker->trial()->videos)
+				vi.create_background_image();
+		}
+
+		background_threshold_ = threshold;
+	}
 }
 
 //Tracking menu

libautoscoper/src/Video.cpp
@@ -60,11 +60,12 @@ using namespace std;
 namespace xromm
 {
 
-Video::Video(const string& dirname)
-    : dirname_(dirname),
-      filenames_(),
-      frame_(),
-      image_(new TiffImage())
+	Video::Video(const string& dirname)
+		: dirname_(dirname),
+		filenames_(),
+		frame_(),
+		image_(new TiffImage()),
+		background_(new TiffImage())
 {
     DIR *dir = opendir(dirname_.c_str());
     if (dir == 0) {
@@ -106,11 +107,13 @@ Video::Video(const Video& video)
       frame_()
 {
     image_ = tiffImageCopy(video.image_);
+	background_ = tiffImageCopy(video.background_);
 }
 
 Video::~Video()
 {
     if (image_) tiffImageFree(image_);
+	if (background_)tiffImageFree(background_);
 }
 
 Video&
@@ -123,10 +126,81 @@ Video::operator=(const Video& video)
     if (image_) tiffImageFree(image_);
     image_ = tiffImageCopy(video.image_);
 
+	if (background_)tiffImageFree(background_);
+	background_ = tiffImageCopy(video.background_);
+
     return *this;
 }
 
-void
+int Video::create_background_image()
+{
+	if (filenames_.size() < 2)
+		return -1;
+
+	TiffImage* tmp_image = new TiffImage();
+	//Read tmp_image
+	TIFFSetWarningHandler(0);
+
+	//Read _background
+	TIFF* tif = TIFFOpen(filenames_.at(0).c_str(), "r");
+	
+	if (!tif) {
+		cerr << "Video::frame(): Unable to open image. " << endl;
+		return -2;
+	}
+
+	tiffImageFree(background_);
+	tiffImageRead(tif, background_);
+	TIFFClose(tif);
+
+	memset(background_->data, 0, background_->dataSize);
+	tif = TIFFOpen("background.tif", "w");
+	tiffImageWrite(tif, background_);
+	TIFFClose(tif);
+	for (int i = 0; i < filenames_.size(); i++){
+		tif = TIFFOpen(filenames_.at(i).c_str(), "r");
+		if (!tif) {
+			cerr << "Video::frame(): Unable to open image. " << endl;
+			return -2;
+		}
+
+		tiffImageFree(tmp_image);
+		tiffImageRead(tif, tmp_image);
+		TIFFClose(tif);
+
+		if (background_->bitsPerSample == 8){
+			unsigned char * ptr = reinterpret_cast<unsigned char*> (tmp_image->data);
+			unsigned char * ptr_b = reinterpret_cast<unsigned char*> (background_->data);
+			for (; ptr < reinterpret_cast<unsigned char*>(tmp_image->data) + tmp_image->dataSize; ptr++, ptr_b++)
+			{
+				if (*ptr > *ptr_b)
+					*ptr_b = *ptr;
+			}
+		}
+		else
+		{
+			unsigned short * ptr = static_cast<unsigned short*> (tmp_image->data);
+			unsigned short * ptr_b = reinterpret_cast<unsigned short*> (background_->data);
+			for (; ptr < reinterpret_cast<unsigned short*>(tmp_image->data) + tmp_image->dataSize; ptr++, ptr_b++)
+			{
+				if (*ptr != 0 && *ptr_b == 0)
+					*ptr_b = 255;
+			}
+		}
+
+		tif = TIFFOpen("background.tif", "w");
+		tiffImageWrite(tif, background_);
+		TIFFClose(tif);
+	}
+
+	tiffImageFree(tmp_image);
+
+	return 1;
+}
+
+
+
+	void
 Video::set_frame(size_type i)
 {
     if (i >= filenames_.size()) {

libautoscoper/src/Video.hpp
@@ -72,6 +72,8 @@ public:
 
     // Accessors
 
+	int create_background_image();
+
     const std::string& dirname() const { return dirname_; }
 
     const std::string& filename(size_type i) const { return filenames_.at(i); }
@@ -101,6 +103,8 @@ private:
     size_type frame_;
 
     TiffImage* image_;
+
+	TiffImage* background_;
 };
 
 } // namespace xromm

autoscoper/src/ui/AutoscoperMainWindow.h
@@ -122,6 +122,9 @@ class AutoscoperMainWindow : public QMainWindow{
 		//WortldView
 		WorldViewWindow * worldview;
 
+		//Threshold
+		double background_threshold_;
+
 		//History
 		History *history;
 		bool first_undo;
@@ -174,6 +177,7 @@ class AutoscoperMainWindow : public QMainWindow{
 		void on_actionCopy_triggered(bool checked);
 		void on_actionPaste_triggered(bool checked);
 		void on_actionDelete_triggered(bool checked);
+		void on_actionSet_Background_triggered(bool checked);
 
 		//Tracking
 		void on_actionImport_triggered(bool checked);

autoscoper/src/ui/ui-files/AutoscoperMainWindow.ui
@@ -6,8 +6,8 @@
    <rect>
     <x>0</x>
     <y>0</y>
-    <width>774</width>
-    <height>695</height>
+    <width>920</width>
+    <height>974</height>
    </rect>
   </property>
   <property name="minimumSize">
@@ -383,8 +383,8 @@ Trial</string>
     <rect>
      <x>0</x>
      <y>0</y>
-     <width>774</width>
-     <height>21</height>
+     <width>920</width>
+     <height>43</height>
     </rect>
    </property>
    <widget class="QMenu" name="menuFile">
@@ -418,6 +418,8 @@ Trial</string>
     <addaction name="actionCopy"/>
     <addaction name="actionPaste"/>
     <addaction name="actionDelete"/>
+    <addaction name="separator"/>
+    <addaction name="actionSet_Background"/>
    </widget>
    <widget class="QMenu" name="menuTracking">
     <property name="title">
@@ -577,9 +579,9 @@ Trial</string>
    </property>
   </action>
   <action name="actionSave_Test_Sequence">
-    <property name="text">
-      <string>Save Test Sequence</string>
-    </property>
+   <property name="text">
+    <string>Save Test Sequence</string>
+   </property>
   </action>
   <action name="actionShow_world_view">
    <property name="checkable">
@@ -587,6 +589,11 @@ Trial</string>
    </property>
    <property name="text">
     <string>Show world view</string>
+   </property>
+  </action>
+  <action name="actionSet_Background">
+   <property name="text">
+    <string>Set Background</string>
    </property>
   </action>
  </widget>

libautoscoper/src/View.cpp
@@ -61,6 +61,7 @@
 #include "gpu/cuda/RayCaster.hpp"
 #include "gpu/cuda/RadRenderer.hpp"
 #include "gpu/cuda/Merger_kernels.h"
+
 #else
 #include "gpu/opencl/Compositor.hpp"
 #include "gpu/opencl/Merger.hpp"
@@ -125,6 +126,9 @@ View::~View()
     delete drrFilterBuffer_;
     delete radBuffer_;
     delete radFilterBuffer_;
+
+	delete backgroundmask_;
+	delete drr_mask_;
 #endif
 	drrBuffer_.clear();
 }
@@ -439,6 +443,11 @@ View::init()
         cutilSafeCall(cudaMalloc((void**)&drrFilterBuffer_, maxWidth_*maxHeight_*sizeof(float)));
         cutilSafeCall(cudaMalloc((void**)&radBuffer_, maxWidth_*maxHeight_*sizeof(float)));
         cutilSafeCall(cudaMalloc((void**)&radFilterBuffer_, maxWidth_*maxHeight_*sizeof(float)));
+		cutilSafeCall(cudaMalloc((void**)&backgroundmask_, maxWidth_*maxHeight_*sizeof(float)));
+		cutilSafeCall(cudaMalloc((void**)&drr_mask_, maxWidth_*maxHeight_*sizeof(float)));
+		// fill the array
+		gpu::fill(backgroundmask_, maxWidth_*maxHeight_, 1.0f);
+		gpu::fill(drr_mask_, maxWidth_*maxHeight_, 1.0f);
     }
 }
 #else
@@ -458,6 +467,10 @@ View::init(unsigned width, unsigned height)
 		drrFilterBuffer_ = new Buffer(maxWidth_*maxHeight_*sizeof(float));
         radBuffer_       = new Buffer(maxWidth_*maxHeight_*sizeof(float));
         radFilterBuffer_ = new Buffer(maxWidth_*maxHeight_*sizeof(float));
+		backgroundmask_  = new Buffer(maxWidth_*maxHeight_*sizeof(float));
+		drr_mask_		 = new Buffer(maxWidth_*maxHeight_*sizeof(float));
+		backgroundmask_->fill(1.0f);
+		drr_mask_->fill(1.0f);
 		inited_ = true;
     }
 }

libautoscoper/src/View.hpp
@@ -144,6 +144,9 @@ private:
     Buffer* radFilterBuffer_;
     Buffer* filterBuffer_;
 
+	Buffer* backgroundmask_;
+	Buffer* drr_mask_;
+
 	bool inited_;
 };
 

libautoscoper/src/gpu/cuda/Compositor_kernels.cu
@@ -41,6 +41,8 @@
 
 #include "Compositor_kernels.h"
 
+#include <thrust/device_ptr.h>
+#include <thrust/fill.h>
 
 // Define the cuda compositiing kernel
 __global__
@@ -66,6 +68,13 @@ void composite(float* src1,
     
     // Call the kernel
     composite_kernel<<<gridDim, blockDim>>>(src1,src2,dest,width,height);
+}
+
+void fill(float* src1, unsigned int size, float val)
+{
+	thrust::device_ptr<float> dev_ptr(src1);
+	thrust::fill(dev_ptr, dev_ptr + size, 1.0);
+
 }
 
 } // namespace gpu

commit 48fcab9734ba44ca101826156a0d57de99891f12

ENH: Generalize background image calculations to support 16/32-bit radiographs (#245)
Refactor the working 8-bit background image calculation into a generic function
to work on 8, 16,  and 32-bit radiograph images. 
Previously, any radiograph that was not 8-bit would use the same broken 16-bit
method.  This 16-bit method failed to correctly loop over the image because the
loop failed to take the size  of the datatype into account. This bug was originally
introduced in commit 9bbce52 ("background mask 1", 2018-04-03).

Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

autoscoper/src/ui/AutoscoperMainWindow.cpp
@@ -902,8 +902,13 @@ void AutoscoperMainWindow::setPose(std::vector<double> pose, unsigned int volume
 void AutoscoperMainWindow::setBackground(double threshold)
 {
   if (background_threshold_ < 0){
-    for (xromm::Video &vi : tracker->trial()->videos)
-      vi.create_background_image();
+    for (xromm::Video& vi : tracker->trial()->videos) {
+      if (!vi.create_background_image()) {
+        std::cerr << "Error creating background image for video " << vi.dirname() << "\n"
+          << "Failed to set background threshold" << std::endl;
+        return;
+      }
+    }
 
     tracker->updateBackground();
   }

libautoscoper/src/Video.cpp
@@ -43,6 +43,9 @@
 #include <algorithm>
 #include <iostream>
 #ifdef WIN32
+#ifndef NOMINMAX // Otherwise windows.h defines min/max macros
+#define NOMINMAX
+#endif // NOMINMAX
 #include <windows.h>
 #include "Win32/dirent.h"
 #elif __APPLE__
@@ -140,61 +143,79 @@ Video::operator=(const Video& video)
     return *this;
 }
 
-int Video::create_background_image()
+bool Video::create_background_image()
 {
-  if (filenames_.size() < 2)
-    return -1;
-
+  if (filenames_.size() < 2) {
+    std::cerr << "Video::create_background_image(): Not enough images to create background image." << std::endl;
+    return false;
+  }
 
   if (background_) delete[] background_;
   background_ = new float[width()*height()];
   memset(background_, 0, width()*height()*sizeof(float));
 
-  //Read tmp_image
-  TiffImage* tmp_image = new TiffImage();
-  TIFFSetWarningHandler(0);
-  TIFF* tif;
+  for (size_t i = 0; i < filenames_.size(); i++){
 
-  for (int i = 0; i < filenames_.size(); i++){
-    tif = TIFFOpen(filenames_.at(i).c_str(), "r");
+    // Read tmp_image
+    TIFFSetWarningHandler(0);
+    TIFF* tif = TIFFOpen(filenames_.at(i).c_str(), "r");
+
     if (!tif) {
-      std::cerr << "Video::frame(): Unable to open image. " << std::endl;
-      return -2;
+      std::cerr << "Video::create_background_image(): Unable to open image: " << filenames_.at(i) << std::endl;
+      return false;
     }
 
-    tiffImageFree(tmp_image);
+    TiffImage* tmp_image = new TiffImage();
     tiffImageRead(tif, tmp_image);
     TIFFClose(tif);
 
-    if (tmp_image->bitsPerSample == 8){
-      unsigned char * ptr = reinterpret_cast<unsigned char*> (tmp_image->data);
-      float* ptr_b = background_;
-      for (; ptr < reinterpret_cast<unsigned char*>(tmp_image->data) + tmp_image->dataSize; ptr++, ptr_b++)
-      {
-        float val = *ptr;
-        if (val / 255 > *ptr_b)
-          *ptr_b = val / 255;
-      }
-    }
-    else
+    switch (tmp_image->bitsPerSample)
     {
-      unsigned short * ptr = static_cast<unsigned short*> (tmp_image->data);
-      float * ptr_b = background_;
-      for (; ptr < reinterpret_cast<unsigned short*>(tmp_image->data) + tmp_image->dataSize; ptr++, ptr_b++)
-      {
-        float val = *ptr;
-        if (val / 65535  > *ptr_b)
-          *ptr_b = val / 65535;
-      }
+    case 8:
+      create_background_image_internal<unsigned char>(tmp_image);
+      tiffImageFree(tmp_image);
+      break;
+    case 16:
+      create_background_image_internal<unsigned short>(tmp_image);
+      tiffImageFree(tmp_image);
+      break;
+    case 32:
+      create_background_image_internal<unsigned int>(tmp_image);
+      tiffImageFree(tmp_image);
+      break;
+    default:
+      std::cerr << "Video::create_background_image(): Unsupported bits per sample." << std::endl;
+      tiffImageFree(tmp_image);
+      return false;
     }
   }
 
-  tiffImageFree(tmp_image);
-
-  return 1;
+  return true;
 }
 
-
+template <typename T> void Video::create_background_image_internal(TiffImage* tmp_img) {
+  static_assert(
+    std::is_same<T, unsigned char>::value
+    || std::is_same<T, unsigned short>::value
+    || std::is_same<T, unsigned int>::value, "T must be of type unsigned char, unsigned short, or unsigned int");
+  constexpr unsigned int normalization_factor = std::numeric_limits<T>::max();
+  static_assert(
+    normalization_factor == 255
+    || normalization_factor == 65535
+    || normalization_factor == 4294967295, "normalization_factor must be one of 255, 65535 or 4294967295");
+  T* start = reinterpret_cast<T*>(tmp_img->data);
+  T* end = start + (tmp_img->dataSize / sizeof(T));
+  T* iter = start;
+  float* bg = background_;
+  while(iter < end) {
+    float val = *iter;
+    if (val / normalization_factor > *bg) {
+      *bg = val / normalization_factor;
+    }
+    iter++;
+    bg++;
+  }
+}
 
   void
 Video::set_frame(size_type i)

libautoscoper/src/Video.hpp
@@ -72,7 +72,7 @@ public:
 
     // Accessors
 
-  int create_background_image();
+  bool create_background_image();
 
     const std::string& dirname() const { return dirname_; }
 
@@ -97,6 +97,7 @@ public:
   const float* background() const { return background_; }
 
 private:
+    template <typename T> void create_background_image_internal(TiffImage* tmp_img);
 
     std::string dirname_;
 