BrownBiomechanics/Autoscoper

commit 866a5d2f3bab52f5f354a62bd8baa7afd15f588e

ENH: Add support for 16 and 32-bit radiograph images
Prior to these changes, the supported CUDA texture types (and by extension
radiograph images) was hard-coded to 8-bit (unsigned char) and managed as
a static global variable.

Since texture references needed to be known at compile time and the current
type was hard-coded to 8-bit, the use of them prevented from being able to
easily support 8, 16, and 32 bit radiograph images known at runtime.

To address this, this commit removes the use of `cudaBindTextureToArray`
(Deprecated since CUDA 11.3 and removed in CUDA 12.x)
and switch to using texture object instead by introducing a convenience helper
function `createTextureObjectFromArray`.

Approach implemented here is based of recommendation the post titled
"Kepler Texture Objects Improve Performance and Flexibility" published
in 2013.
See https://developer.nvidia.com/blog/cuda-pro-tip-kepler-texture-objects-improve-performance-and-flexibility/

For reference, before this changes, the error message reported at build time
while attempting to use autoscoper built against CUDA 12.x was:

```
3 Errors under file path `...\libautoscoper\src\gpu\cuda\RadRenderer_kernels.cu:
- no instance of overloaded function "tex2D" matches the argument list
- identifier "cudaBindTextureToArray" is undefined
- texture is not a template
```

libautoscoper/src/gpu/cuda/cutil/cutil_create_tex_obj.h
@@ -1,0 +1,26 @@
+#pragma once
+#include <cuda.h>
+#include <cutil_inline.h>
+#include <cutil_inline_runtime.h>
+
+inline cudaTextureObject_t createTexureObjectFromArray(cudaArray* arr, cudaTextureReadMode readMode) {
+    // Approach implemented below is based off of
+    // https://developer.nvidia.com/blog/cuda-pro-tip-kepler-texture-objects-improve-performance-and-flexibility/
+
+    cudaResourceDesc resDesc;
+    memset(&resDesc, 0, sizeof(resDesc));
+    resDesc.resType = cudaResourceTypeArray;
+    resDesc.res.array.array = arr;
+
+    cudaTextureDesc texDesc;
+    memset(&texDesc, 0, sizeof(texDesc));
+    texDesc.normalizedCoords = true;
+    texDesc.filterMode = cudaFilterModeLinear;
+    texDesc.addressMode[0] = cudaAddressModeClamp;
+    texDesc.addressMode[1] = cudaAddressModeClamp;
+    texDesc.readMode = readMode;
+
+    cudaTextureObject_t tex = 0;
+    cutilSafeCall(cudaCreateTextureObject(&tex, &resDesc, &texDesc, NULL));
+    return tex;
+}

libautoscoper/src/gpu/cuda/BackgroundRenderer.cpp
@@ -43,6 +43,7 @@
 #include <sstream>
 
 #include <cuda.h>
+#include <cutil_create_tex_obj.h>
 #include <cutil_inline.h>
 #include <cutil_math.h>
 
@@ -111,8 +112,9 @@ BackgroundRenderer::set_viewport(float x, float y, float width, float height)
 void
 BackgroundRenderer::render(float* buffer, size_t width, size_t height, float threshold) const
 {
-    background_bind_array(array_);
-    background_render(buffer,
+    cudaTextureObject_t tex = createTexureObjectFromArray(array_, cudaReadModeElementType);
+
+    background_render(tex,buffer,
                  (int)width,
                  (int)height,
                  image_plane_[0],
@@ -124,6 +126,8 @@ BackgroundRenderer::render(float* buffer, size_t width, size_t height, float thr
                  viewport_[2],
                  viewport_[3],
                  threshold);
+
+    cudaDestroyTextureObject(tex);
 }
 
 } } // namespace xromm::cuda

libautoscoper/src/gpu/cuda/BackgroundRenderer_kernels.cu
@@ -43,14 +43,10 @@
 
 #include <cutil_inline.h>
 
-/////// Global Variables ////////
-
-static texture<float,2> tex;
-
 //////// Image Rendering Kernel ////////
 
 __global__
-void background_render_kernel(float* output, int width, int height, float u0,
+void background_render_kernel(cudaTextureObject_t tex,float* output, int width, int height, float u0,
                          float v0, float u1, float v1, float u2, float v2,
              float u3, float v3, float threshold);
 
@@ -60,20 +56,8 @@ namespace xromm
 namespace gpu
 {
 
-void background_bind_array(const cudaArray* array)
-{
-    // Setup 2D texture.
-    tex.normalized = true;
-    tex.filterMode = cudaFilterModeLinear;
-    tex.addressMode[0] = cudaAddressModeClamp;
-    tex.addressMode[1] = cudaAddressModeClamp;
 
-    // Bind array to 3D texture.
-    cutilSafeCall(cudaBindTextureToArray(tex, array));
-}
-
-
-void background_render(float* output, int width, int height, float u0,
+void background_render(cudaTextureObject_t tex,float* output, int width, int height, float u0,
                   float v0, float u1, float v1, float u2, float v2,
                   float u3, float v3, float threshold)
 {
@@ -82,7 +66,7 @@ void background_render(float* output, int width, int height, float u0,
     dim3 gridDim((width+blockDim.x-1)/blockDim.x,
                  (height+blockDim.y-1)/blockDim.y);
 
-  background_render_kernel << <gridDim, blockDim >> >(output, width, height,
+  background_render_kernel << <gridDim, blockDim >> >(tex,output, width, height,
                                                u0, v0, u1, v1, u2, v2,
                                                u3, v3, threshold);
 }
@@ -92,7 +76,7 @@ void background_render(float* output, int width, int height, float u0,
 } // namespace xromm
 
 __global__
-void background_render_kernel(float* output, int width, int height, float u0,
+void background_render_kernel(cudaTextureObject_t tex,float* output, int width, int height, float u0,
                          float v0, float u1, float v1, float u2, float v2,
              float u3, float v3, float threshold)
 {
@@ -113,7 +97,7 @@ void background_render_kernel(float* output, int width, int height, float u0,
         output[width*y+x] = 0.0f;
     }
     else {
-    output[width*y + x] = (threshold <= tex2D(tex, s, t)) ? 1.0f : 0.0f;
+    output[width*y + x] = (threshold <= tex2D<float>(tex, s, t)) ? 1.0f : 0.0f;
     }
 }
 

libautoscoper/src/gpu/cuda/BackgroundRenderer_kernels.h
@@ -48,9 +48,8 @@ namespace xromm
 namespace gpu
 {
 
-void background_bind_array(const cudaArray* array);
 
-void background_render(float* output, int width, int height, float u0,
+void background_render(cudaTextureObject_t tex,float* output, int width, int height, float u0,
                   float v0, float u1, float v1, float u2, float v2,
                   float u3, float v3, float threshold);
 

libautoscoper/src/gpu/cuda/Compositor_kernels.cu
@@ -67,8 +67,8 @@ void composite(float* src1,
 {
     // Calculate the block and grid sizes.
     dim3 blockDim(32, 32);
-    dim3 gridDim((width+blockDim.x-1)/blockDim.x,
-                 (height+blockDim.y-1)/blockDim.y);
+    dim3 gridDim(((unsigned int)width+blockDim.x-1)/blockDim.x,
+                 ((unsigned int)height+blockDim.y-1)/blockDim.y);
 
     // Call the kernel
     composite_kernel<<<gridDim, blockDim>>>(src1,src2,src3,src4,dest,width,height);

libautoscoper/src/gpu/cuda/DRRBackground_kernels.cu
@@ -58,8 +58,8 @@ namespace xromm {
 {
     // Calculate the block and grid sizes.
     dim3 blockDim(32, 32);
-    dim3 gridDim((width+blockDim.x-1)/blockDim.x,
-                 (height+blockDim.y-1)/blockDim.y);
+    dim3 gridDim(((unsigned int)width+blockDim.x-1)/blockDim.x,
+                 ((unsigned int)height+blockDim.y-1)/blockDim.y);
 
     // Call the kernel
   drr_background_kernel << <gridDim, blockDim >> >(src1, dest, width, height);

libautoscoper/src/gpu/cuda/Merger_kernels.cu
@@ -60,8 +60,8 @@ void merge(float* src1,
 {
     // Calculate the block and grid sizes.
     dim3 blockDim(32, 32);
-    dim3 gridDim((width+blockDim.x-1)/blockDim.x,
-                 (height+blockDim.y-1)/blockDim.y);
+    dim3 gridDim(((unsigned int)width+blockDim.x-1)/blockDim.x,
+                 ((unsigned int)height+blockDim.y-1)/blockDim.y);
 
     // Call the kernel
     merge_kernel<<<gridDim, blockDim>>>(src1,src2,dest,width,height);

libautoscoper/src/gpu/cuda/Mult_kernels.cu
@@ -60,8 +60,8 @@ namespace xromm {
     {
       // Calculate the block and grid sizes.
       dim3 blockDim(32, 32);
-      dim3 gridDim((width + blockDim.x - 1) / blockDim.x,
-        (height + blockDim.y - 1) / blockDim.y);
+      dim3 gridDim(((unsigned int)width + blockDim.x - 1) / blockDim.x,
+        ((unsigned int)height + blockDim.y - 1) / blockDim.y);
 
       // Call the kernel
       mult_kernel<<<gridDim, blockDim>>>(src1, src2, dest, width, height);

commit 51882e1ac4b53f399a895806439a5777ff8480b2

BUG: Fix rendering of 3D texture associated with the CUDA backend
Fix #264 by addressing a regression introduced in 866a5d2 ("ENH: Add support
for 16 and 32-bit radiograph images", 2023-01-13)

This ensures the texture clamping mode is consistently set across all
dimensions.

Co-authored-by: David Allemang <david.allemang@kitware.com>

libautoscoper/src/gpu/cuda/cutil/cutil_create_tex_obj.h
@@ -19,6 +19,7 @@ inline cudaTextureObject_t createTexureObjectFromArray(cudaArray* arr, cudaTextu
   texDesc.filterMode = cudaFilterModeLinear;
   texDesc.addressMode[0] = cudaAddressModeClamp;
   texDesc.addressMode[1] = cudaAddressModeClamp;
+  texDesc.addressMode[2] = cudaAddressModeClamp;
   texDesc.readMode = readMode;
 
   cudaTextureObject_t tex = 0;