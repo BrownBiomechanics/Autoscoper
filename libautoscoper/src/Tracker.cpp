BrownBiomechanics/Autoscoper

commit 4d2eec33f2e60ad4e81031832915891c690862db

ENH: Update interpolation to use Quaternions rather than eulers (#255)
Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

libautoscoper/src/Tracker.cpp
@@ -357,16 +357,21 @@ void Tracker::optimize(int frame,
   int framesBehind = (dFrame > 0) ? (int)trial_.frame : (int)trial_.num_frames - trial_.frame - 1;
 
   if (trial_.guess == 2 && framesBehind > 1) {
-    double xyzypr1[6] = {
-      (*trial_.getXCurve(-1))(trial_.frame - 2 * dFrame),     (*trial_.getYCurve(-1))(trial_.frame - 2 * dFrame),
-      (*trial_.getZCurve(-1))(trial_.frame - 2 * dFrame),     (*trial_.getYawCurve(-1))(trial_.frame - 2 * dFrame),
-      (*trial_.getPitchCurve(-1))(trial_.frame - 2 * dFrame), (*trial_.getRollCurve(-1))(trial_.frame - 2 * dFrame)
-    };
-    double xyzypr2[6] = {
-      (*trial_.getXCurve(-1))(trial_.frame - dFrame),     (*trial_.getYCurve(-1))(trial_.frame - dFrame),
-      (*trial_.getZCurve(-1))(trial_.frame - dFrame),     (*trial_.getYawCurve(-1))(trial_.frame - dFrame),
-      (*trial_.getPitchCurve(-1))(trial_.frame - dFrame), (*trial_.getRollCurve(-1))(trial_.frame - dFrame)
-    };
+    float x_val = (*(const_cast<Trial&>(trial_)).getXCurve(-1))(trial_.frame - 2 * dFrame);
+    float y_val = (*(const_cast<Trial&>(trial_)).getYCurve(-1))(trial_.frame - 2 * dFrame);
+    float z_val = (*(const_cast<Trial&>(trial_)).getZCurve(-1))(trial_.frame - 2 * dFrame);
+    Quatf quat_val = (*(const_cast<Trial&>(trial_)).getQuatCurve(-1))(trial_.frame - 2 * dFrame);
+    Vec3f eulers = quat_val.toEuler();
+
+    double xyzypr1[6] = { x_val, y_val, z_val, eulers.z, eulers.y, eulers.x };
+
+    x_val = (*(const_cast<Trial&>(trial_)).getXCurve(-1))(trial_.frame - dFrame);
+    y_val = (*(const_cast<Trial&>(trial_)).getYCurve(-1))(trial_.frame - dFrame);
+    z_val = (*(const_cast<Trial&>(trial_)).getZCurve(-1))(trial_.frame - dFrame);
+    quat_val = (*(const_cast<Trial&>(trial_)).getQuatCurve(-1))(trial_.frame - dFrame);
+    eulers = quat_val.toEuler();
+
+    double xyzypr2[6] = { x_val, y_val, z_val, eulers.z, eulers.y, eulers.x };
 
     CoordFrame xcframe = CoordFrame::from_xyzypr(xyzypr1).linear_extrap(CoordFrame::from_xyzypr(xyzypr2));
 
@@ -374,25 +379,33 @@ void Tracker::optimize(int frame,
     trial_.getXCurve(-1)->insert(trial_.frame, xyzypr1[0]);
     trial_.getYCurve(-1)->insert(trial_.frame, xyzypr1[1]);
     trial_.getZCurve(-1)->insert(trial_.frame, xyzypr1[2]);
-    trial_.getYawCurve(-1)->insert(trial_.frame, xyzypr1[3]);
-    trial_.getPitchCurve(-1)->insert(trial_.frame, xyzypr1[4]);
-    trial_.getRollCurve(-1)->insert(trial_.frame, xyzypr1[5]);
+    trial_.getQuatCurve(-1)->insert(trial_.frame, Quatf(xyzypr1[3], xyzypr1[4], xyzypr1[5]));
   } else if (trial_.guess == 1 && framesBehind > 0) {
-    trial_.getXCurve(-1)->insert(trial_.frame, (*trial_.getXCurve(-1))(trial_.frame - dFrame));
-    trial_.getYCurve(-1)->insert(trial_.frame, (*trial_.getYCurve(-1))(trial_.frame - dFrame));
-    trial_.getZCurve(-1)->insert(trial_.frame, (*trial_.getZCurve(-1))(trial_.frame - dFrame));
-    trial_.getYawCurve(-1)->insert(trial_.frame, (*trial_.getYawCurve(-1))(trial_.frame - dFrame));
-    trial_.getPitchCurve(-1)->insert(trial_.frame, (*trial_.getPitchCurve(-1))(trial_.frame - dFrame));
-    trial_.getRollCurve(-1)->insert(trial_.frame, (*trial_.getRollCurve(-1))(trial_.frame - dFrame));
+    float x_val = (*(const_cast<Trial&>(trial_)).getXCurve(-1))(trial_.frame - dFrame);
+    float y_val = (*(const_cast<Trial&>(trial_)).getYCurve(-1))(trial_.frame - dFrame);
+    float z_val = (*(const_cast<Trial&>(trial_)).getZCurve(-1))(trial_.frame - dFrame);
+    Quatf quat_val = (*(const_cast<Trial&>(trial_)).getQuatCurve(-1))(trial_.frame - dFrame);
+    Vec3f eulers = quat_val.toEuler();
+
+    double xyzypr[6] = { x_val, y_val, z_val, eulers.z, eulers.y, eulers.x };
+
+    trial_.getXCurve(-1)->insert(trial_.frame, xyzypr[0]);
+    trial_.getYCurve(-1)->insert(trial_.frame, xyzypr[1]);
+    trial_.getZCurve(-1)->insert(trial_.frame, xyzypr[2]);
+    trial_.getQuatCurve(-1)->insert(trial_.frame, Quatf(xyzypr[3], xyzypr[4], xyzypr[5]));
   }
 
   int totalIter = 0;
   for (int j = 0; j < repeats; j++) {
 
     // Get Current Pose
-    double xyzypr[6] = { (*trial_.getXCurve(-1))(trial_.frame),     (*trial_.getYCurve(-1))(trial_.frame),
-                         (*trial_.getZCurve(-1))(trial_.frame),     (*trial_.getYawCurve(-1))(trial_.frame),
-                         (*trial_.getPitchCurve(-1))(trial_.frame), (*trial_.getRollCurve(-1))(trial_.frame) };
+    float x_val = (*(const_cast<Trial&>(trial_)).getXCurve(-1))(trial_.frame - dFrame);
+    float y_val = (*(const_cast<Trial&>(trial_)).getYCurve(-1))(trial_.frame - dFrame);
+    float z_val = (*(const_cast<Trial&>(trial_)).getZCurve(-1))(trial_.frame - dFrame);
+    Quatf quat_val = (*(const_cast<Trial&>(trial_)).getQuatCurve(-1))(trial_.frame - dFrame);
+    Vec3f eulers = quat_val.toEuler();
+
+    double xyzypr[6] = { x_val, y_val, z_val, eulers.z, eulers.y, eulers.x };
 
     // Init Manip for saving final optimum
     double init_manip[6] = { 0 };
@@ -442,9 +455,7 @@ void Tracker::optimize(int frame,
       trial_.getXCurve(-1)->insert(trial_.frame, xyzypr[0]);
       trial_.getYCurve(-1)->insert(trial_.frame, xyzypr[1]);
       trial_.getZCurve(-1)->insert(trial_.frame, xyzypr[2]);
-      trial_.getYawCurve(-1)->insert(trial_.frame, xyzypr[3]);
-      trial_.getPitchCurve(-1)->insert(trial_.frame, xyzypr[4]);
-      trial_.getRollCurve(-1)->insert(trial_.frame, xyzypr[5]);
+      trial_.getQuatCurve(-1)->insert(trial_.frame, Quatf(xyzypr[3], xyzypr[4], xyzypr[5]));
 
       // DOWNHILL SIMPLEX
       // Generate the 7 vertices that form the initial simplex. Because
@@ -525,9 +536,7 @@ void Tracker::optimize(int frame,
     trial_.getXCurve(-1)->insert(trial_.frame, xyzypr[0]);
     trial_.getYCurve(-1)->insert(trial_.frame, xyzypr[1]);
     trial_.getZCurve(-1)->insert(trial_.frame, xyzypr[2]);
-    trial_.getYawCurve(-1)->insert(trial_.frame, xyzypr[3]);
-    trial_.getPitchCurve(-1)->insert(trial_.frame, xyzypr[4]);
-    trial_.getRollCurve(-1)->insert(trial_.frame, xyzypr[5]);
+    trial_.getQuatCurve(-1)->insert(trial_.frame, Quatf(xyzypr[3], xyzypr[4], xyzypr[5]));
   }
   totalIter += ITER;
 
@@ -735,13 +744,13 @@ std::vector<double> Tracker::trackFrame(unsigned int volumeID, double* xyzypr) c
 double Tracker::minimizationFunc(const double* values) const
 {
   // Construct a coordinate frame from the given values
+  float x_val = (*(const_cast<Trial&>(trial_)).getXCurve(-1))(trial_.frame);
+  float y_val = (*(const_cast<Trial&>(trial_)).getYCurve(-1))(trial_.frame);
+  float z_val = (*(const_cast<Trial&>(trial_)).getZCurve(-1))(trial_.frame);
+  Quatf quat_val = (*(const_cast<Trial&>(trial_)).getQuatCurve(-1))(trial_.frame);
+  Vec3f eulers = quat_val.toEuler();
 
-  double xyzypr[6] = { (*(const_cast<Trial&>(trial_)).getXCurve(-1))(trial_.frame),
-                       (*(const_cast<Trial&>(trial_)).getYCurve(-1))(trial_.frame),
-                       (*(const_cast<Trial&>(trial_)).getZCurve(-1))(trial_.frame),
-                       (*(const_cast<Trial&>(trial_)).getYawCurve(-1))(trial_.frame),
-                       (*(const_cast<Trial&>(trial_)).getPitchCurve(-1))(trial_.frame),
-                       (*(const_cast<Trial&>(trial_)).getRollCurve(-1))(trial_.frame) };
+  double xyzypr[6] = { x_val, y_val, z_val, eulers.z, eulers.y, eulers.x };
   CoordFrame xcframe = CoordFrame::from_xyzypr(xyzypr);
 
   CoordFrame manip = CoordFrame::from_xyzAxis_angle(values);
@@ -967,13 +976,12 @@ bool Tracker::calculate_viewport(const CoordFrame& modelview, const Camera& came
 // Save Full DRR Image
 void Tracker::getFullDRR(unsigned int volumeID) const
 {
-  double xyzypr[6] = { (*(const_cast<Trial&>(trial_)).getXCurve(-1))(trial_.frame),
-                       (*(const_cast<Trial&>(trial_)).getYCurve(-1))(trial_.frame),
-                       (*(const_cast<Trial&>(trial_)).getZCurve(-1))(trial_.frame),
-                       (*(const_cast<Trial&>(trial_)).getYawCurve(-1))(trial_.frame),
-                       (*(const_cast<Trial&>(trial_)).getPitchCurve(-1))(trial_.frame),
-                       (*(const_cast<Trial&>(trial_)).getRollCurve(-1))(trial_.frame) };
-
+  float x_val = (*(const_cast<Trial&>(trial_)).getXCurve(-1))(trial_.frame);
+  float y_val = (*(const_cast<Trial&>(trial_)).getYCurve(-1))(trial_.frame);
+  float z_val = (*(const_cast<Trial&>(trial_)).getZCurve(-1))(trial_.frame);
+  Quatf quat_val = (*(const_cast<Trial&>(trial_)).getQuatCurve(-1))(trial_.frame);
+  Vec3f eulers = quat_val.toEuler();
+  double xyzypr[6] = { x_val, y_val, z_val, eulers.z, eulers.y, eulers.x };
   CoordFrame xcframe = CoordFrame::from_xyzypr(xyzypr);
 
   for (unsigned int i = 0; i < views_.size(); ++i) {
@@ -1018,4 +1026,5 @@ void Tracker::getFullDRR(unsigned int volumeID) const
     save_full_drr(rendered_drr_, render_width, render_height);
   }
 }
+
 } // namespace xromm

.gitignore
@@ -12,6 +12,10 @@ _make_installer/*
 .vs/
 CMakeSettings.json
 
+# Nvim
+compile_commands.json
+.cache/
+
 # PyCharm
 out/
 

autoscoper/src/core/History.hpp
@@ -49,12 +49,10 @@
 
 struct State
 {
-  std::vector<KeyCurve> x_curve;
-  std::vector<KeyCurve> y_curve;
-  std::vector<KeyCurve> z_curve;
-  std::vector<KeyCurve> x_rot_curve;
-  std::vector<KeyCurve> y_rot_curve;
-  std::vector<KeyCurve> z_rot_curve;
+  std::vector<KeyCurve<float>> x_curve;
+  std::vector<KeyCurve<float>> y_curve;
+  std::vector<KeyCurve<float>> z_curve;
+  std::vector<KeyCurve<Quatf>> quat_curve;
 };
 
 class History

autoscoper/src/ui/GLTimeline.h
@@ -49,7 +49,7 @@ namespace xromm {
 class Trial;
 }
 using xromm::Trial;
-class KeyCurve;
+class IKeyCurve;
 struct GraphData;
 
 class GLTimeline : public GLWidget
@@ -76,7 +76,8 @@ private:
   bool modify_nodes;
   bool glutIsInitialised;
   void mouse_to_graph(double mouse_x, double mouse_y, double& graph_x, double& graph_y);
-  void draw_curve(const KeyCurve& curve);
+  void draw_curve(const IKeyCurve* icurve);
+  void draw_curve_quat(const IKeyCurve* curve, bool show_yaw, bool show_pitch, bool show_roll);
   void render_bitmap_string(double x, double y, const char* string);
   void renderText(double textPosX, double textPosY, QString text);
   float round_this(double my_val);

autoscoper/src/ui/TimelineDockWidget.cpp
@@ -361,31 +361,29 @@ void TimelineDockWidget::update_graph_min_max(int frame)
         position_graph->min_value = z_value;
       }
     }
+    Vec3f eulers = (*mainwindow->getTracker()->trial()->getQuatCurve(-1))(frame).toEuler();
     if (position_graph->show_yaw) {
-      float yaw_value = (*mainwindow->getTracker()->trial()->getYawCurve(-1))(frame);
-      if (yaw_value > position_graph->max_value) {
-        position_graph->max_value = yaw_value;
+      if (eulers.z > position_graph->max_value) {
+        position_graph->max_value = eulers.z;
       }
-      if (yaw_value < position_graph->min_value) {
-        position_graph->min_value = yaw_value;
+      if (eulers.z < position_graph->min_value) {
+        position_graph->min_value = eulers.z;
       }
     }
     if (position_graph->show_pitch) {
-      float pitch_value = (*mainwindow->getTracker()->trial()->getPitchCurve(-1))(frame);
-      if (pitch_value > position_graph->max_value) {
-        position_graph->max_value = pitch_value;
+      if (eulers.y > position_graph->max_value) {
+        position_graph->max_value = eulers.y;
       }
-      if (pitch_value < position_graph->min_value) {
-        position_graph->min_value = pitch_value;
+      if (eulers.y < position_graph->min_value) {
+        position_graph->min_value = eulers.y;
       }
     }
     if (position_graph->show_roll) {
-      float roll_value = (*mainwindow->getTracker()->trial()->getRollCurve(-1))(frame);
-      if (roll_value > position_graph->max_value) {
-        position_graph->max_value = roll_value;
+      if (eulers.x > position_graph->max_value) {
+        position_graph->max_value = eulers.x;
       }
-      if (roll_value < position_graph->min_value) {
-        position_graph->min_value = roll_value;
+      if (eulers.x < position_graph->min_value) {
+        position_graph->min_value = eulers.x;
       }
     }
   }
@@ -395,73 +393,52 @@ void TimelineDockWidget::update_graph_min_max(int frame)
     position_graph->min_value = 1e6;
     position_graph->max_value = -1e6;
 
-    if (position_graph->show_x) {
-      for (frame = floor(position_graph->min_frame); frame < position_graph->max_frame; frame += 1.0f) {
-        float x_value = (*mainwindow->getTracker()->trial()->getXCurve(-1))(frame);
-        if (x_value > position_graph->max_value) {
-          position_graph->max_value = x_value;
-        }
-        if (x_value < position_graph->min_value) {
-          position_graph->min_value = x_value;
-        }
-      }
-    }
-    if (position_graph->show_y) {
-      for (frame = floor(position_graph->min_frame); frame < position_graph->max_frame; frame += 1.0f) {
-        float y_value = (*mainwindow->getTracker()->trial()->getYCurve(-1))(frame);
-        if (y_value > position_graph->max_value) {
-          position_graph->max_value = y_value;
-        }
-        if (y_value < position_graph->min_value) {
-          position_graph->min_value = y_value;
-        }
-      }
-    }
-    if (position_graph->show_z) {
-      for (frame = floor(position_graph->min_frame); frame < position_graph->max_frame; frame += 1.0f) {
-        float z_value = (*mainwindow->getTracker()->trial()->getZCurve(-1))(frame);
-        if (z_value > position_graph->max_value) {
-          position_graph->max_value = z_value;
-        }
-        if (z_value < position_graph->min_value) {
-          position_graph->min_value = z_value;
-        }
-      }
-    }
-    if (position_graph->show_yaw) {
-      for (frame = floor(position_graph->min_frame); frame < position_graph->max_frame; frame += 1.0f) {
-        float yaw_value = (*mainwindow->getTracker()->trial()->getYawCurve(-1))(frame);
-        if (yaw_value > position_graph->max_value) {
-          position_graph->max_value = yaw_value;
-        }
-        if (yaw_value < position_graph->min_value) {
-          position_graph->min_value = yaw_value;
-        }
-      }
-    }
-    if (position_graph->show_pitch) {
-      for (frame = floor(position_graph->min_frame); frame < position_graph->max_frame; frame += 1.0f) {
-        float pitch_value = (*mainwindow->getTracker()->trial()->getPitchCurve(-1))(frame);
-        if (pitch_value > position_graph->max_value) {
-          position_graph->max_value = pitch_value;
-        }
-        if (pitch_value < position_graph->min_value) {
-          position_graph->min_value = pitch_value;
-        }
-      }
-    }
-    if (position_graph->show_roll) {
-      for (frame = floor(position_graph->min_frame); frame < position_graph->max_frame; frame += 1.0f) {
-        float roll_value = (*mainwindow->getTracker()->trial()->getRollCurve(-1))(frame);
-        if (roll_value > position_graph->max_value) {
-          position_graph->max_value = roll_value;
-        }
-        if (roll_value < position_graph->min_value) {
-          position_graph->min_value = roll_value;
-        }
-      }
-    }
+    double min_max[2] = { 1e6, -1e6 };
 
+    for (frame = floor(position_graph->min_frame); frame < position_graph->max_frame; frame += 1.0f) {
+      if (position_graph->show_x) {
+        float x_value = (*mainwindow->getTracker()->trial()->getXCurve(-1))(frame);
+        if (x_value < min_max[0])
+          min_max[0] = x_value;
+        if (x_value > min_max[1])
+          min_max[1] = x_value;
+      }
+      if (position_graph->show_y) {
+        float y_value = (*mainwindow->getTracker()->trial()->getYCurve(-1))(frame);
+        if (y_value < min_max[0])
+          min_max[0] = y_value;
+        if (y_value > min_max[1])
+          min_max[1] = y_value;
+      }
+      if (position_graph->show_z) {
+        float z_value = (*mainwindow->getTracker()->trial()->getZCurve(-1))(frame);
+        if (z_value < min_max[0])
+          min_max[0] = z_value;
+        if (z_value > min_max[1])
+          min_max[1] = z_value;
+      }
+      Vec3f eulers = (*mainwindow->getTracker()->trial()->getQuatCurve(-1))(frame).toEuler();
+      if (position_graph->show_yaw) {
+        if (eulers.z < min_max[0])
+          min_max[0] = eulers.z;
+        if (eulers.z > min_max[1])
+          min_max[1] = eulers.z;
+      }
+      if (position_graph->show_pitch) {
+        if (eulers.y < min_max[0])
+          min_max[0] = eulers.y;
+        if (eulers.y > min_max[1])
+          min_max[1] = eulers.y;
+      }
+      if (position_graph->show_roll) {
+        if (eulers.x < min_max[0])
+          min_max[0] = eulers.x;
+        if (eulers.x > min_max[1])
+          min_max[1] = eulers.x;
+      }
+    }
+    position_graph->min_value = min_max[0];
+    position_graph->max_value = min_max[1];
     position_graph->min_value -= 1.0;
     position_graph->max_value += 1.0;
   }

autoscoper/src/ui/TimelineDockWidget.h
@@ -89,16 +89,16 @@ public:
   void setFramesRange(int firstFrame, int lastFrame);
   GraphData* getPosition_graph() { return position_graph; }
   AutoscoperMainWindow* getMainWindow() { return mainwindow; };
-  std::vector<std::pair<std::pair<KeyCurve*, KeyCurve::iterator>, Selection_type>>* getSelectedNodes()
+  std::vector<std::pair<std::pair<IKeyCurve*, IKeyCurve::iterator>, Selection_type>>* getSelectedNodes()
   {
     return &selected_nodes;
   }
-  void setSelectedNodes(std::vector<std::pair<std::pair<KeyCurve*, KeyCurve::iterator>, Selection_type>> new_nodes)
+  void setSelectedNodes(std::vector<std::pair<std::pair<IKeyCurve*, IKeyCurve::iterator>, Selection_type>> new_nodes)
   {
     selected_nodes = new_nodes;
   }
 
-  std::vector<std::pair<KeyCurve*, KeyCurve::iterator>>* getCopiedNodes() { return &copied_nodes; }
+  std::vector<std::pair<IKeyCurve*, IKeyCurve::iterator>>* getCopiedNodes() { return &copied_nodes; }
 
   void getValues(double* xyzypr);
   void setValues(double* xyzypr);
@@ -118,8 +118,8 @@ private:
   AutoscoperMainWindow* mainwindow;
   bool m_spinButtonUpdate;
 
-  std::vector<std::pair<std::pair<KeyCurve*, KeyCurve::iterator>, Selection_type>> selected_nodes;
-  std::vector<std::pair<KeyCurve*, KeyCurve::iterator>> copied_nodes;
+  std::vector<std::pair<std::pair<IKeyCurve*, IKeyCurve::iterator>, Selection_type>> selected_nodes;
+  std::vector<std::pair<IKeyCurve*, IKeyCurve::iterator>> copied_nodes;
 
   // Video play
   int play_tag;

commit 8f9c68831c2d633e1b0155f3973b75dda2e61d79

BUG: Fix incorrect application of input heuristics
Regression introduced in 4d2eec3 ("ENH: Update interpolation to
use Quaternions rather than eulers (#255)", 2024-02-29)

libautoscoper/src/Tracker.cpp
@@ -399,10 +399,10 @@ void Tracker::optimize(int frame,
   for (int j = 0; j < repeats; j++) {
 
     // Get Current Pose
-    float x_val = (*(const_cast<Trial&>(trial_)).getXCurve(-1))(trial_.frame - dFrame);
-    float y_val = (*(const_cast<Trial&>(trial_)).getYCurve(-1))(trial_.frame - dFrame);
-    float z_val = (*(const_cast<Trial&>(trial_)).getZCurve(-1))(trial_.frame - dFrame);
-    Quatf quat_val = (*(const_cast<Trial&>(trial_)).getQuatCurve(-1))(trial_.frame - dFrame);
+    float x_val = (*(const_cast<Trial&>(trial_)).getXCurve(-1))(trial_.frame);
+    float y_val = (*(const_cast<Trial&>(trial_)).getYCurve(-1))(trial_.frame);
+    float z_val = (*(const_cast<Trial&>(trial_)).getZCurve(-1))(trial_.frame);
+    Quatf quat_val = (*(const_cast<Trial&>(trial_)).getQuatCurve(-1))(trial_.frame);
     Vec3f eulers = quat_val.toEuler();
 
     double xyzypr[6] = { x_val, y_val, z_val, eulers.z, eulers.y, eulers.x };